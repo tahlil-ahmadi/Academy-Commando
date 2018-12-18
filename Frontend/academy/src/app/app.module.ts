ee236570a2017ae858a092dded3d02eadafe42fb
Teacher1 <Teacher1@CLASS1>
2018-12-18 20:09:40 +0330

    loading bar added

--- Frontend/academy/src/app/app.module.ts
+++ Frontend/academy/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AcademyModule } from './academy/academy.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AcademyModule,
     RouterModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LoadingBarHttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
